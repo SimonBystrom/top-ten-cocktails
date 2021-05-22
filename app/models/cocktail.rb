104e6b3b5b67c31e89140e9935f42710339f56f1
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-22 15:04:59 +0900

    Seeded db with one example and crash tested model structure

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,4 @@
 class Cocktail < ApplicationRecord
+  has_many :recipes
   has_many :ingredients, through: :recipes
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts '... creating cocktail seeds'
+@old_fashioned = Cocktail.new(name: 'old_fashioned', time: 3, instructions: 'Pour whiskey into glass and put in ice')
+@old_fashioned.save
+@whiskey = Ingredient.new(name: 'Whiskey')
+@whiskey.save
+
+@old_fashioned_recipe = Recipe.new(cocktail: @old_fashioned, ingredient: @whiskey, amount: '30 cl')
+@old_fashioned_recipe.save
+puts "created #{Cocktail.all.count} number of cocktails"
