ad05f62daef74f47da4958ec3d2752d5a7ec1acb
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-23 21:53:34 +0900

    Added routes for filtered options in ingredeints controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
   root to: 'pages#home'
-  resources :cocktails, only: [:index, :show]
+  resources :cocktails, only: [:index, :show] do
+  end
+      resources :ingredients, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
